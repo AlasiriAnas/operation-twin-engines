9ecfdf24bcdd0662c8a2d7dd300db0f8f2ae31f3
AlasiriAnas <almanaanas1@gmail.com>
2026-04-16 00:55:59 +0300

    Fix VPC peering self_link issue

--- modules/network/peering.tf
+++ modules/network/peering.tf
@@ -1,11 +1,11 @@
 resource "google_compute_network_peering" "frontend_to_backend" {
   name         = "frontend-to-backend"
-  network      = google_compute_network.frontend.name
+  network      = google_compute_network.frontend.self_link
   peer_network = google_compute_network.backend.self_link
 }
 
 resource "google_compute_network_peering" "backend_to_frontend" {
   name         = "backend-to-frontend"
-  network      = google_compute_network.backend.name
+  network      = google_compute_network.backend.self_link
   peer_network = google_compute_network.frontend.self_link
 }
